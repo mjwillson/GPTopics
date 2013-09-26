567e2c8ee78fc835e32d875fe84ce8451ad9316c
Matthew Willson <matthew@swiftkey.net>
2013-09-26 19:41:11 +0100

    WIP

--- gptopics.jl
+++ gptopics.jl
@@ -25,8 +25,10 @@
 # sigma2 -- noise variance (scalar)
 # Kappa - DxD kernel matrix over documents computed from X and tau
 
-function kernelmatrix(X, tau)
-    return #...
+function gaussian_kernel_matrix(data, tau)
+    function(i :: Int, j :: Int)
+        exp(- sum((data[i,:] - data[j,:]) .^ 2) / (2*tau^2))
+    end
 end
 
 function fitmodel(W :: SparseMatrixCSC{Uint32,Uint32},
@@ -96,12 +98,12 @@ function fitmodel(W :: SparseMatrixCSC{Uint32,Uint32},
         # -= first deriv / second deriv
         Lambda_deriv = -s * (Lambda - F) + sum_phi - N_over_Zeta_exp_lambda_nu
         Lambda_2deriv = -s - N_over_Zeta_exp_lambda_nu
-        Lambda -= Lambda_deriv / Lambda_2deriv
+        Lambda -= Lambda_deriv ./ Lambda_2deriv
 
         # Coordinate-wise newton Update for Nu^2
         Nu2_deriv = -s/2 - N_over_Zeta_exp_lambda_nu/2 + Nu2/2
         Nu2_deriv2 = -N_over_Zeta_exp_lambda_nu/4 - (Nu2 .^ 2)/2
-        Nu2 -= Nu2_deriv / Nu2_deriv2
+        Nu2 -= Nu2_deriv ./ Nu2_deriv2
 
         # Update for the noise precision s (simpler than for the full covariance Sigma)
         # (TODO: we could learn separate diagonal entries for the noise covariance, might give an ARD effect. worthwhile?)
@@ -187,7 +189,131 @@ function fitmodel(W :: SparseMatrixCSC{Uint32,Uint32},
         exp_F_N_over_Zeta = exp_F .* (doc_totals ./ Zeta)
         F_deriv = - (F / Kappa) + sum_phi - exp_F_N_over_Zeta
         F_deriv2 = - exp_F_N_over_Zeta + diag(inv(Kappa)) # TODO get this from cholesky and use cholesky above too
-        F -= F_deriv / F_deriv2
+        F -= F_deriv ./ F_deriv2
+
+        # TODO: convergence check?
+    end
+end
+
+
+# Even simpler still in which there's just one latent topic per document.
+# This is a mixture model not an admixture model.
+# The topic/word distributions are integrated out here and we use a collapsed
+# variational bayes approach to inference
+function fitmodel(W :: SparseMatrixCSC{Uint32,Uint32},
+                  X :: Matrix{Float32},
+                  K :: Uint32,
+                  tau :: Float32,
+                  sigma2 :: Float32,
+                  T :: Int # dimensionality of low-rank kernel approx
+                  )
+    (V,D) = size(W)
+    @assert size(X) == (D, 2)
+
+    doc_words = Array(Vector{Uint32}, D)
+    doc_wordcounts = Array(Vector{Uint32}, D)
+    doc_totals :: Vector{Uint32} = zeros(D)
+    for d = 1:D
+        colrange = W.colptr[d] : W.colptr[d+1]-1
+        doc_words[d] = W.rowval[colrange]
+        doc_wordcounts[d] = W.nzval[colrange]
+        doc_totals[d] = length(colrange)
+    end
+
+    # Approx cholesky decomp of kernel matrix.
+    # Actual kernel once white noise added is L^T L + sigma^2 I.
+    # But we use woodbury identities etc, don't directly compute it.
+    L = ichol(gaussian_kernel_matrix(X, tau), D, T)
+
+    M = (1/sigma2) * L*L'
+    M[diagind(M)] += 1
+    B = chol(M)
+    C = L' / B
+    # so now K^-1 approx= sigma^-2 I - sigma^-4 C C^T.
+    # we precompute the diagonal of K^-1 for use in newton updates:
+    K_inv_diag = sigma2^-1 - sigma2^-2 * sum(C .^ 2, 2)
+
+    F :: Matrix{Float32} = zeros(D,K)
+    Zeta :: Vector{Float32} = zeros(D)
+    Q :: Matrix{Float32} = zeros(D,K)
+
+    while true
+
+        # Update for beta
+        Beta[:,:] = 0
+        for d = 1:D
+            wc = doc_wordcounts[d]
+            w = doc_words[d]
+            # todo rewrite this as a dense by sparse mx mult?
+            for i = 1:length(w)
+                Beta[:, w[i]] += wc[i] * Phi[d,:]
+            end
+        end
+        Beta ./= sum(Beta, 2)
+
+        # Update for Phi
+        for d = 1:D
+            w = doc_words[d]
+            for i = 1:length(w)
+                p = exp(F[d,:]) .* Beta[:, w[i]]
+                Phi[d,:] = p
+                Phi[d,:] /= sum(p)
+            end
+        end
+
+        # Update for Zeta
+        exp_F = exp(F)
+        Zeta = sum(exp_F, 2)
+        exp_F_over_Zeta = exp_F ./ Zeta
+
+        # Update for F -- no exact form this time. A componentwise newton
+        # (i.e. which approximates the hessian by its diagonal) update is fairly quick:
+
+        invK_F = sigma2^-1 * F - sigma2^-2 * (C * (C' * F))
+        F_gradient = - invK_F + Q - exp_F_over_Zeta
+        F_hessian_diags = - K_inv_diag - exp_F_over_Zeta
+
+        F -= F_gradient ./ F_hessian_diags
+
+        # Full newton updates for F would be O(K D T^2)
+        # as opposed to O(KDT) for componentwise newton:
+        if false
+            for k in 1:K
+                Lambda_inv = 1 ./ (exp_F_over_Zeta[:,k] + sigma2^-1)
+                Lambda_inv_Fgradk = Lambda_inv .* F_gradient[:,k]
+
+                update = Lambda_inv .* F_gradient[:,k]
+                update = L * update
+                update = (M + (L.*Lambda_inv)*L') \ update # will \ use cholesky here?
+                update = L' * update
+                update = F_gradient[:, k] + update
+                update = Lambda_inv .* update
+                F[:,k] += update
+            end
+        end
+
+        # Update for Q
+        sums = zeros(K,W)
+        for d = 1:D
+            sums[:, doc_words[d]] += Q[d,:] * doc_wordcounts[d]
+        end
+        margin_sums = sum(sums, 2)
+
+        for d = 1:D
+            sums[:, doc_words[d]] -= Q[d,:] * doc_wordcounts[d]
+            margin_sums[d] -= sum(Q[d,:] * doc_wordcounts[d])
+
+            # words in D by K, then sum along words to get K
+            log_qd = sum(gammaln(beta + doc_wordcounts[d] + sums), 1) -
+              gammaln(V*beta + doc_totals[d] + margin_sums) +
+              F[d,:]
+
+            Q[d,:] = softmax(log_qd)
+
+            # after updating Q[d,:], add it back:
+            sums[:, doc_words[d]] -= Q[d,:] * doc_wordcounts[d]
+            margin_sums[d] += sum(Q[d,:] * doc_wordcounts[d])
+        end
 
         # TODO: convergence check?
     end
